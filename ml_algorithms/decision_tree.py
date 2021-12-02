c1ae1130fead31c48c73f13e32b069be8276edab
Himanshu Bag <himanshubag12@gmail.com>
2021-12-02 12:22:31 +0530

    updated decison_tree.py with proper working code

--- ml_algorithms/decision_tree.py
+++ ml_algorithms/decision_tree.py
@@ -2,20 +2,13 @@
 Decison Tree Implementation from scratch.
 Important points : 
         - create a tree ds to store data
-        - how to make rules for split
-        -  
+        - how to make rules for split , remember those splits
 By - 0x1h0b
 
 '''
 
-
-from collections import Counter
-from numpy.core.defchararray import split
-import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-from scipy.sparse.construct import random
-import math
+import pandas as pd
 
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
@@ -53,100 +46,170 @@ def entropy(y):
     return -result
 
 
+# class Node for tree
 class Node:
-    def __init__(self,feature=None,threshold=None,left=None,right=None,*,value=None):
-        self.feature = feature
+    def __init__(self,feature_index=None,threshold=None,left=None,right=None,info_gain=None,entropy=None,value=None):
+        self.feature_index = feature_index
         self.threshold = threshold
         self.left = left
         self.right = right
+        self.info_gain = info_gain
+        self.entropy = entropy
         self.value = value
-    
-    def is_leaf_node(self):
-        return self.value is not None
 
-
-class DecisionTree:
-    def __init__(self,min_sample_splits=2,max_depth=100,n_feature = None):
-        self.min_sample_splits = min_sample_splits
-        self.max_depth = max_depth
-        self.n_feature = n_feature
+# decision tree classifier class
+class myDecisionTreeClassifier():
+    def __init__(self, min_samples_split=2, max_depth=2,feature_list=[],target_list=[]):
+        ''' constructor '''
+        # initialize the root of the tree 
         self.root = None
-    
-    def fit(self,x,y):
-        self.n_feature = x.shape[1] if not self.n_feature else min(self.n_feature,x.shape[1])
-        self.root = self._grow_tree(x,y)
-
-    def _grow_tree(self,x,y,depth=0):
-        n_sample,n_feature = x.shape
-        n_labels = len(np.unique(y))
-
-        # stopping criteria
-        if depth >= self.max_depth or n_labels ==1 or n_sample<self.min_sample_splits:
-            leaf_node = self._most_common_label(y)
-            return Node(value=leaf_node)
-
-        feature_index = np.random.choice(n_feature,self.n_feature,replace=False)
-
-        best_feature , best_threshold = self._best_criteria(x,y,feature_index)
-        left_idx,right_idx = self._split(x[:,best_feature],best_threshold)
-
-        left = self._grow_tree(x[left_idx,:],y[left_idx],depth+1)
-        right = self._grow_tree(x[right_idx,:],y[right_idx],depth+1)
-
-        return Node(best_feature,best_threshold,left,right)
-
-    
-    def _best_criteria(self,x,y,feature_index):
-        best_gain = -1
-        split_idx ,split_threshold = None,None
-        for idx in feature_index:
-            x_col = x[:,idx]
-            thresholds = np.unique(x_col)
-            for thres in thresholds:
-                gain = self._information_gain(y,x_col,thres)
-
-                if gain > best_gain:
-                    best_gain = gain
-                    split_idx = idx
-                    split_threshold = thres
-        return split_idx,split_threshold
-
-    def _information_gain(self,y,x_col,split_threshold):
-        parent_entropy = entropy(y)
-        left_idx, right_idx = self._split(x_col,split_threshold)
-
-        if len(left_idx)==0 or len(right_idx)==0:
-            return 0
+        # stopping conditions
+        self.min_samples_split = min_samples_split
+        self.max_depth = max_depth
         
-        n=len(y)
-        n_l,n_r = len(left_idx),len(right_idx)
-        e_l ,e_r = entropy(y[left_idx]),entropy(y[right_idx])
-        child_entropy = (n_l/n)*e_l + (n_r/n)*e_r
-
-        return parent_entropy - child_entropy
-
+        # feature list and target lists
+        self.feature_list = feature_list
+        self.target_list = target_list
+        
+    def build_tree(self, dataset, curr_depth=0):
+        ''' recursive function to build the tree ''' 
+        X, Y = dataset[:,:-1], dataset[:,-1]
+        num_samples, num_features = np.shape(X)
+        
+        # printing the current node metadata of tree
+        print('\nLevel ',curr_depth)
+        val,count = np.unique(Y,return_counts=True)
+#         print(val,count)
+#         print('Y:',Y)
+#         print(dataset)
+        val,count = [int(i) for i in val],[int(j) for j in count]
+        for idx in range(len(val)):
+#             print(int(uv),type(uv))
+            print('Count of '+str(self.target_list[val[idx]])+' = '+str(count[idx]))
+        
+        # split until stopping conditions are met
+        if num_samples>=self.min_samples_split and curr_depth<=self.max_depth:
+            # find the best split
+            best_split = self.get_best_split(dataset, num_samples, num_features)
+            # check if information gain is positive
+            if best_split["info_gain"]>0:
+                # print the node parameters
+                gain_ratio = best_split["info_gain"]/best_split["entropy_curr_node"]
+                print('Current Entropy is = '+str(best_split["entropy_curr_node"]))
+                print('Splitting on feature '+str(self.feature_list[best_split["feature_index"]])+' with gain ratio = '+str(gain_ratio))
+                
+                # recur left
+                left_subtree = self.build_tree(best_split["dataset_left"], curr_depth+1)
+                # recur right
+                right_subtree = self.build_tree(best_split["dataset_right"], curr_depth+1)
+                # return decision node
+                return Node(best_split["feature_index"], best_split["threshold"],left_subtree, right_subtree, best_split["info_gain"],best_split["entropy_curr_node"])
+        
+        # compute leaf node
+        print('Current entropy is = 0.0')
+        print('Reached leaf node')
+        leaf_value = self.calculate_leaf_value(Y)
+        # return leaf node
+        return Node(value=leaf_value)
     
-    def _split(self,x_col,split_thres):
-        left_idx = np.argwhere(x_col<=split_thres).flatten()
-        right_idx = np.argwhere(x_col > split_thres).flatten()
-        return left_idx,right_idx
-
-
-    def predict(self,X):
-        return np.array([self._traverse_tree(x,self.root) for x in X])
+    def get_best_split(self, dataset, num_samples, num_features):
+        ''' function to find the best split '''
+        
+        # dictionary to store the best split
+        best_split = {}
+        max_info_gain = -float("inf")
+        
+        # loop over all the features
+        for feature_index in range(num_features):
+            feature_values = dataset[:, feature_index]
+            possible_thresholds = np.unique(feature_values)
+            # loop over all the feature values present in the data
+            for threshold in possible_thresholds:
+                # get current split
+                dataset_left, dataset_right = self.split(dataset, feature_index, threshold)
+                # check if childs are not null
+                if len(dataset_left)>0 and len(dataset_right)>0:
+                    y, left_y, right_y = dataset[:, -1], dataset_left[:, -1], dataset_right[:, -1]
+                    # compute information gain
+                    curr_info_gain, curr_entropy = self.information_gain(y, left_y, right_y, "gini")
+                    # update the best split if needed
+                    if curr_info_gain>max_info_gain:
+                        best_split["feature_index"] = feature_index
+                        best_split["threshold"] = threshold
+                        best_split["dataset_left"] = dataset_left
+                        best_split["dataset_right"] = dataset_right
+                        best_split["info_gain"] = curr_info_gain
+                        best_split["entropy_curr_node"] = curr_entropy
+                        max_info_gain = curr_info_gain
+                        
+        return best_split
     
-    def _traverse_tree(self,x,node):
-        if node.is_leaf_node():
-            return node.value
+    def split(self, dataset, feature_index, threshold):
+        ''' function to split the data '''
+        dataset_left = np.array([row for row in dataset if row[feature_index]<=threshold])
+        dataset_right = np.array([row for row in dataset if row[feature_index]>threshold])
+        return dataset_left, dataset_right
+    
+    def information_gain(self, parent, l_child, r_child, mode="entropy"):
+        ''' function to compute information gain '''
+        
+        weight_l = len(l_child) / len(parent)
+        weight_r = len(r_child) / len(parent)
+        entropy_parent = self.entropy(parent)
+        if mode=="gini":
+            gain = self.gini_index(parent) - (weight_l*self.gini_index(l_child) + weight_r*self.gini_index(r_child))
+        else:
+            gain = entropy_parent - (weight_l*self.entropy(l_child) + weight_r*self.entropy(r_child))
+        return gain,entropy_parent
+    
+    def entropy(self, y):
+        ''' function to compute entropy '''
+        class_labels = np.unique(y)
+        entropy = 0
+        for cls in class_labels:
+            p_cls = len(y[y == cls]) / len(y)
+            entropy += -p_cls * np.log2(p_cls)
+        return entropy
+    
+    def gini_index(self, y):
+        ''' function to compute gini index '''
+#         print('y =>',y)
+        class_labels = np.unique(y)
+#         print('here')
+        gini = 0
+        for cls in class_labels:
+            p_cls = len(y[y == cls]) / len(y)
+            gini += p_cls**2
+        return 1 - gini
+    
+    def get_unique_counts(self,y):
+        l = np.unique(y)
+        print()
+    def calculate_leaf_value(self, Y):
+        ''' function to compute leaf node '''
+        Y = list(Y)
+        return max(Y, key=Y.count)
 
-        if x[node.feature] <= node.threshold:
-            return self._traverse_tree(x,node.left)
-        return self._traverse_tree(x,node.right)
+    def fit(self, X, Y):
+        ''' function to train the tree '''
+        dataset = np.concatenate((X, Y), axis=1)
+        self.root = self.build_tree(dataset)
+    
+    def predict(self, X):
+        ''' function to predict new dataset '''
+        preditions = [self.make_prediction(x, self.root) for x in X]
+        return preditions
+    
+    def make_prediction(self, x, tree):
+        ''' function to predict a single data point '''
+        if tree.value!=None: return tree.value
+        feature_val = x[tree.feature_index]
+        if feature_val<=tree.threshold:
+            return self.make_prediction(x, tree.left)
+        else:
+            return self.make_prediction(x, tree.right)
 
-    def _most_common_label(self,y):
-        counter = Counter(y)
-        most_common = counter.most_common(1)[0][0]
-        return most_common
+
 
 def report(y_test,y_pred):
     print('\n\n\t-:: CLASSIFICATION SUMMARY ::-')
@@ -162,31 +225,34 @@ def report(y_test,y_pred):
     print('\nClassification report :-\n',report)
 
 
-
+# dataset loading
 data = datasets.load_breast_cancer()
 x = data.data
-y = data.target
+y = data.target.reshape(-1,1)
+t_list = data.target_names
+f_list = data.feature_names
+
+
+# # iris dataset
+# iris_data = pd.read_csv('iris.csv')
+# iris_data['species'].value_counts()
+# dd={
+#     'Iris-setosa':0,
+#     'Iris-versicolor':1,
+#     'Iris-virginica':2
+# }
+# iris_data['species'].replace(dd,inplace=True)
+# f_list = ['sepal_length','sepal_width','petal_length','petal_width']
+# t_list = list(dd.keys())
+# print(iris_data.info())
+# x = iris_data.iloc[:, :-1].values
+# y = iris_data.iloc[:, -1].values.reshape(-1,1)
+
 
 
 x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=1234)
-
-from sklearn.tree import DecisionTreeClassifier
-clf = DecisionTreeClassifier()
+clf = myDecisionTreeClassifier(min_samples_split=3, max_depth=3,feature_list = f_list,target_list=t_list)
 clf.fit(x_train,y_train)
 y_pred = clf.predict(x_test)
-# clf = DecisionTree(max_depth=12)
-# clf.fit(x_train,y_train)
-
-# y_pred = clf.predict(x_test)
 
 report(y_test,y_pred)
-
-
-from sklearn.tree import export_graphviz
-import pydotplus
-
-xx = export_graphviz(clf,out_file=None)
-xy = pydotplus.graph_from_dot_file(xx)
-xy.write_pdf('demo.pdf')
-
-
